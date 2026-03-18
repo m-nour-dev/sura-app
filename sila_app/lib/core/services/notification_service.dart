db6bc31de4a91e6bb0cc41a769827ee31d962bb4
Hamed Mohamed <hamedmohamed22w@gmail.com>
2026-03-18 16:21:49 +0300

    fix: resolve missing PrefsService import in NotificationService

--- sila_app/lib/core/services/notification_service.dart
+++ sila_app/lib/core/services/notification_service.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:audioplayers/audioplayers.dart';
+import 'package:sila_app/core/services/prefs_service.dart';
 
 class NotificationService {
   static final NotificationService _instance = NotificationService._internal();
